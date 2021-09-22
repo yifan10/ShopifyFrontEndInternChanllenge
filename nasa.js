5701e7312dd5d556f7a2841e7edb358035030e39
Fifi12345 <yifan.ding@mail.mcgill.ca>
2021-09-21 23:16:35 -0400

    Add the start_date function

--- nasa.js
+++ nasa.js
@@ -1,20 +1,14 @@
-//"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=500&api_key="
-//https://api.nasa.gov/planetary/apod?api_key=
-sendApiRequest("first");
-sendApiRequest("second");
+let dataOne = sendApiRequest("first");
+let dataTwo = sendApiRequest("second");
 async function sendApiRequest(temp) {
   const username = "tNm4ifUUtta2vX0AwsqvWE7aCnGkhKHGrVbyVfcU";
   const response = await fetch(
-    "https://api.nasa.gov/planetary/apod?api_key=" + username
+    "https://api.nasa.gov/planetary/apod?api_key=" + username + "&count=1"
   );
-  if (temp == "second") {
-    const response = await fetch(
-      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=" +
-        username
-    );
-  }
-  const data = await response.json();
+  const dataArray = await response.json();
+  const data = dataArray[0];
   useApiData(data, temp);
+  return data;
 }
 
 function useApiData(data, temp) {
@@ -27,31 +21,52 @@ function useApiData(data, temp) {
 
 function myFunction(temp) {
   const name = "#" + temp + "B";
-  if (document.querySelector(name).innerHTML == "Liked") {
+  if (document.querySelector(name).innerHTML == "Unlike") {
     document.querySelector(name).innerHTML = "Like";
   } else {
-    document.querySelector(name).innerHTML = "Liked";
+    document.querySelector(name).innerHTML = "Unlike";
   }
 }
 const facebook = document.querySelector("#facebook");
-
+const today = new Date();
+console.log(today.get);
 
 async function share(temp) {
-    const username = "tNm4ifUUtta2vX0AwsqvWE7aCnGkhKHGrVbyVfcU";
-    const response = await fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=" + username
-    );
-    if (temp == "second") {
-      const response = await fetch(
-        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=" +
-          username
-      );
-    }
-    const data = await response.json();
-    const shareUrl = 'http://www.facebook.com/sharer/sharer.php?u='+data.url;
-    window.open(shareUrl, "NewWindow");
+  let url = "";
+  if (temp == "first") {
+    url = (await dataOne).url;
+  } else {
+    url = (await dataTwo).url;
+  }
+  const shareUrl = "http://www.facebook.com/sharer/sharer.php?u=" + url;
+  window.open(shareUrl, "NewWindow");
 }
+function submit() {
+  const startDate = document.querySelector("#startdate").value;
+  sendApiRequestDate(startDate);
+}
+async function sendApiRequestDate(startDate) {
+  const username = "tNm4ifUUtta2vX0AwsqvWE7aCnGkhKHGrVbyVfcU";
+  const today = new Date();
+  const response = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=" +
+      username +
+      "&start_date=" +
+      startDate +
+      "&end_date=" +
+      today.getFullYear() +
+      "-" +
+      (today.getUTCMonth() + 1) +
+      "-" +
+      today.getDate()
+  );
 
-//document.getElementById("firstButton").onclick = function () {
-//  myFunction();
-//};
+  const dataArray = await response.json();
+  dataOne = dataArray[0];
+  dataTwo = dataArray[0];
+  if (dataArray.length != 1) {
+    dataTwo = dataArray[1];
+  }
+  useApiData(dataOne, "first");
+  useApiData(dataTwo, "second");
+}
